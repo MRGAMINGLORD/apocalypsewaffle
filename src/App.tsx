e2c871c46269db61c6e59f8e4b8ce9257a592de5
MRGAMINGLORD <mrgaminglordfuzz@gmail.com>
2026-03-07 15:17:29 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/apocalypsewaffle">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/login" element={<Login />} />
